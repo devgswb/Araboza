fdc189e58572e974ddf99d447929cdbc88e93f27
Jo Seong Min <erpsm92@gmail.com>
2019-10-11 16:14:48 +0900

    추가: 타이틀 기반 검색 기능

--- src/araboza/backend/modules/senti_analysis/analysis.py
+++ src/araboza/backend/modules/senti_analysis/analysis.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from datetime import date, timedelta
+import collections
 import json
 import pprint
 import pymongo
@@ -8,6 +9,8 @@ import datetime
 import os
 
 class SentiAnalysis:
+    def test(self, site_code, search_word):
+        return self.result_from_db_By_Title("2019-08-25", "2019-09-25", site_code, search_word)
 
     def __make_polarity_dict__(self):
         res_data = {}
@@ -36,7 +39,7 @@ class SentiAnalysis:
             self.db_live_data = data['db_live_data']
 
     def result_from_db(self, start_date, end_date, site_code, search_word):
-        return self.result_from_db_by_date(start_date, end_date, site_code, search_word)
+        return self.result_from_db_By_Title(start_date, end_date, site_code, search_word)
 
     def result_from_db_by_date(self, start_date, end_date, site_code, search_word):
         # start_date의 양식은 2019-01-01
@@ -120,15 +123,101 @@ class SentiAnalysis:
                 'Success': 1
             }
 
+# 여기서부턴 타이틀 기반 검색
+    def search_by_title_by_date(self, data, collection):
+    # 입력 양식은 {
+    #   year: $year
+    #   month: $month
+    #   day : $day,
+    #   site_code: $site_code,
+    #   search_word: $search_word
+    # }
+        year = data['year']
+        month = data['month']
+        day = data['day']
+        site_code = data['site_code']
+        search_word = data['search_word']
+    # 입력 부분 인자값 받아오기
+        result = {
+            'site_code': site_code,
+            'search_word': search_word,
+            'related_words': [],
+            'positive': 0,
+            'negative': 0,
+            'Success': 0,
+            'word_freq_by_date': 0,
+            'date': ''
+        }
+    # 반환 값 생성
+        pipelines = list()
+        pipelines.append({"$unwind": "$data"})
+        pipelines.append({"$match": {
+            "code": site_code,
+            "year": year,
+            "month": month,
+            "day": day,
+            "data.title": {"$regex": f"{search_word}"}}
+        })
+        pipelines.append({"$project": {
+            "title": "$data.title", "words": "$data.words"}
+        })
+        try:
+            rs = list(collection.aggregate(pipelines))
+            # print(len(rs))
+            # rs = collection.find({
+            #     'code': site_code,
+            #     'time': {'$lte': end_date, '$gte': start_date},
+            #     # 'data.words': {'$elemMatch': {'morpheme': search_word}} 형태소 기반 검색
+            #     'data.title': {'$regex': search_word} # 제목 기반 검색
+            # })
+            # return rs
+            total_sentence_count = len(rs)
+            result['word_freq_by_date'] = total_sentence_count
+            date = f'{year}-{month:02d}-{day:02d}'
+            result['date'] = date
+            total_related_words = collections.Counter()
+            if total_sentence_count:
+                count = 0
+                # pp = pprint.PrettyPrinter(indent=4)
+                # pp.pprint(rs)
+                for record in rs:
+                    related_words = {}
+                    title = record['title']
+                    sentence = record['words']
+                    word_list = []  # 한 문장의 단어들을 묶은 것
+                    for word in sentence:
+                        word_list.append(word['morpheme'])
+                    analysis_result = self.__analysis__(word_list)
+                    result['positive'] += analysis_result['positive']
+                    result['negative'] += analysis_result['negative']
+                    # 긍부정 판단
+                    noun = ['NNP', 'NNG', 'NNB']
+                    # print(title, count, total_sentence_count) # 테스트용
+                    for word in sentence:
+                        noun_word = word['morpheme']  # 명사 단어
+                        part_of_word = word['type']  # 품사
+                        if len(noun_word) == 1:
+                            continue
+                        if part_of_word in noun:
+                            if not (noun_word in related_words):
+                                related_words[noun_word] = 1
+                            else:
+                                related_words[noun_word] += 1
+                    total_related_words.update(related_words)
+                result['related_words'] = total_related_words
+                return result
+            else:
+                result['Success'] = 1
+                return result
+        except:
+            result['Success'] = 1
+            return result
+
+
     def result_from_db_By_Title(self, start_date, end_date, site_code, search_word):
         # start_date의 양식은 2019-01-01
         # end_date의 양식은 2019-01-01
-        pos = 0.0  # positive
-        neg = 0.0  # negative
-        word_freq_by_date = {} # 단어 빈도수
         date_list = get_dates(start_date, end_date)
-        for insert_date in date_list:
-            word_freq_by_date[insert_date] = 0
         start_date = start_date.split('-')
         start_date = datetime.datetime(int(start_date[0]), int(start_date[1]), int(start_date[2]))
         end_date = end_date.split('-')
@@ -138,50 +227,46 @@ class SentiAnalysis:
         conn = pymongo.MongoClient(f'mongodb://{username}:{password}@{self.db_host}:{self.db_port}/{self.db_name}')
         db = conn.get_database(self.db_name)
         collection = db[self.db_live_data]
-        rs = collection.find({
-            'code': site_code,
-            'time': {'$lte': end_date, '$gte': start_date},
-            # 'data.words': {'$elemMatch': {'morpheme': search_word}} 형태소 기반 검색
-            'data.title': {'$regex': search_word} # 제목 기반 검색
-        })
-
-        total_sentence_count = 0
-        related_words = {}
-        for record in rs:
-            sentence = record['data']
-            record_year = record['year']
-            record_month = record['month']
-            record_day = record['day']
-            date = f'{record_year}-{record_month:02d}-{record_day:02d}'
-            if not date in word_freq_by_date:
-                word_freq_by_date[date] = 1
-            else:
-                word_freq_by_date[date] += 1
-            word_list = []  # 한 문장의 단어들을 묶은 것
-            for word in sentence['words']:
-                word_list.append(word['morpheme'])
-            analysis_result = self.__analysis__(word_list)
-            pos += analysis_result['positive']
-            neg += analysis_result['negative']
-            # 긍부정 판단
-            total_sentence_count += 1
-            noun = ['NNP', 'NNG']
-            for word in sentence['words']:
-                noun_word = word['morpheme']  # 명사 단어
-                part_of_word = word['type']  # 품사
-                if part_of_word in noun:
-                    if not (noun_word in related_words):
-                        related_words[noun_word] = 0
-                    else:
-                        related_words[noun_word] += 1
-        sorted_words = sorted(related_words.items(), key=lambda x: x[1], reverse=True)
-        # 정렬된 결과 가장 많이 나온 단어는 무조건 검색된 단어니까 첫번째 인덱스는 제외한 [1:6]
-        conn.close()
-        try:
-            return {
+        result_list = []
+        for date in date_list:
+            YEAR = 0
+            MONTH = 1
+            DAY = 2
+            date_raw = date.split('-')
+            year = int(date_raw[YEAR])
+            month = int(date_raw[MONTH])
+            day = int(date_raw[DAY])
+            input_data = {
+                'year': year,
+                'month': month,
+                'day': day,
                 'site_code': site_code,
+                'search_word': search_word
+            }
+            result_data = self.search_by_title_by_date(input_data, collection)
+            result_list.append(result_data)
+        conn.close()
+        # 반환 데이터 정의 파트
+        related_words = collections.Counter()
+        pos = 0.0  # positive
+        neg = 0.0  # negative
+        total_sentence_count = 0
+        word_freq_by_date = {}  # 단어 빈도수
+        #데이터 가공 파트
+        for data_by_date in result_list:
+            date = data_by_date['date']
+            total_sentence_count += data_by_date['word_freq_by_date']
+            word_freq_by_date[date] = data_by_date['word_freq_by_date']
+            if data_by_date['Success'] == 1:
+                continue
+            pos += data_by_date['positive']
+            neg += data_by_date['negative']
+            related_words.update(data_by_date['related_words'])
+        sorted_words = sorted(related_words.items(), key=lambda x: x[1], reverse=True)
+        result = {
+            'site_code': site_code,
                 'search_word': search_word,
-                'related_words': [data for data in sorted_words[1:20] if data[1] > 10],
+                'related_words': [data for data in sorted_words[1:20] if data[1] > 1],
                 'total_sentence_count': total_sentence_count,
                 'positive': pos,
                 'positive_percentage': round((pos) / (pos + neg) * 100, 1),
@@ -189,7 +274,15 @@ class SentiAnalysis:
                 'negative_percentage': round((neg) / (pos + neg) * 100, 1),
                 'Success': 0,
                 'word_freq_by_date': [[data, word_freq_by_date[data]] for data in word_freq_by_date]
+        }
+        if result['total_sentence_count'] == 0:
+            return {
+                'site_code': site_code,
+                'Success': 1
             }
+        # 정렬된 결과 가장 많이 나온 단어는 무조건 검색된 단어니까 첫번째 인덱스는 제외한 [1:6]
+        try:
+            return result
         except:
             return {
                 'site_code': site_code,
@@ -220,8 +313,10 @@ def get_dates(start_date, end_date):
     dates = [s_date + timedelta(days=x) for x in range((e_date - s_date).days + 1)]
     result_dates_list = [ str(data).split(' ')[0] for data in dates ]
     return result_dates_list
-#
+# #
 # pp = pprint.PrettyPrinter(indent=4)
-# pp.pprint(get_dates('2019-08-01', '2019-09-22'))
 # sa = SentiAnalysis()
+# result = sa.test(12, '유게이')
+#
+# pp.pprint(result)
 # print(sa.result_from_db('2019-08-01', '2019-09-25', 8, "결혼"))
